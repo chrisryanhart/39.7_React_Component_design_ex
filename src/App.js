6929368c20ab4f8897ada0f466bc05beb9b3c53f
rrghiker <chris.ryan.hart1@gmail.com>
2022-11-01 16:43:06 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+function App() {
+  return (
+    <div className="App">
+      <p>Test</p>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
